695bd4ea00dbe24d2fe5b8f927525b00bf1724f3
Marcelo <marcelotmendes.ads@gmail.com>
2026-04-09 20:30:20 -0300

    feat(api): create axios instance with auth interceptors

---
+++ src/services/apiClient.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+declare global {
+  interface Window {
+    Clerk?: {
+      session?: {
+        getToken: () => Promise<string | null>;
+      };
+    };
+  }
+}
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+instance.interceptors.request.use(async (config) => {
+  const token = await window.Clerk?.session?.getToken();
+
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
