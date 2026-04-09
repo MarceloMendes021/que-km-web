cdc8cea94d30282721a00df5ad69fb64cd95e3f7
Marcelo <marcelotmendes.ads@gmail.com>
2026-04-09 10:57:46 -0300

    feat(auth): add ClerkProvider to app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
+const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkKey) throw new Error("...");
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
