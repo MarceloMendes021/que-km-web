d969fc8157dd0d59303a416b4bc7c045e5c7309e
Marcelo <marcelotmendes.ads@gmail.com>
2026-04-10 01:59:58 -0300

    feat(providers): add QueryClientProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { AppProviders } from "./app/providers/AppProviders.tsx";
 
 const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,7 +12,9 @@ if (!clerkKey) throw new Error("...");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={clerkKey}>
-      <App />
+      <AppProviders>
+        <App />
+      </AppProviders>
     </ClerkProvider>
   </StrictMode>,
 );
