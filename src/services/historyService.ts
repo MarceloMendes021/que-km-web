7e254fb82c420701f85ca7dd7b0e55220f465ec0
Marcelo <marcelotmendes.ads@gmail.com>
2026-04-18 16:04:13 -0300

    fix(history): map backend snake_case fields to WorkdayHistory type

--- src/services/historyService.ts
+++ src/services/historyService.ts
@@ -2,9 +2,33 @@ import api from "@/services/apiClient";
 import type { WorkdayHistory } from "@/features/history/utils/historyUtils";
 import { calculateWorkdayMetrics } from "@/features/history/utils/historyUtils";
 
+interface WorkdayRaw {
+  id: string;
+  date: string;
+  start_odometer: number;
+  end_odometer: number;
+  earnings_uber: number | string;
+  earnings_99: number | string;
+  earnings_particular: number | string;
+  expenses_fuel?: number | string;
+  expenses_other?: number | string;
+}
+
 export async function getWorkdayHistory(month: string) {
   const response = await api.get("/api/workdays", { params: { month } });
-  return response.data;
+  return response.data.map((w: WorkdayRaw) => ({
+    id: w.id,
+    date: w.date.split("T")[0],
+    initialOdometer: w.start_odometer,
+    finalOdometer: w.end_odometer,
+    earnings: {
+      uber: Number(w.earnings_uber) || 0,
+      "99": Number(w.earnings_99) || 0,
+      particular: Number(w.earnings_particular) || 0,
+    },
+    fuel: Number(w.expenses_fuel) || 0,
+    otherExpenses: Number(w.expenses_other) || 0,
+  }));
 }
 
 export { calculateWorkdayMetrics };
